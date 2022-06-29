dd13c73ec3dabe6581aefa5b36fea89f0611c812
Dennis Renz <dennis@r-renz.de>
2022-06-29 23:35:41 +0200

    Testing some stuff

--- NodaTimeDemo/Program.cs
+++ NodaTimeDemo/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NodaTime;
+using NodaTime.Extensions;
+using NodaTime.Text;
+
+var start = LocalDateTimePattern.GeneralIso.Parse("2022-06-29T23:10:00").Value;
+
+var zone = DateTimeZoneProviders.Tzdb["Europe/Berlin"];
+var clock = SystemClock.Instance.InZone(zone);
+var end = clock.GetCurrentLocalDateTime();
+
+var diff = Period.Between(start, end);
+
+Console.WriteLine($"Start is {start}");
+Console.WriteLine($"End is {end}");
+Console.WriteLine($"Diff is {diff.ToDuration()}");
