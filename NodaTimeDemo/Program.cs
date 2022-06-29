4d0799de8349c3e8bb178b3f12065817740087a8
Dennis Renz <dennis@r-renz.de>
2022-06-30 00:08:46 +0200

    Add demo with parsed utc time

--- NodaTimeDemo/Program.cs
+++ NodaTimeDemo/Program.cs
@@ -4,14 +4,20 @@ using NodaTime.Text;
 
 var start = LocalDateTimePattern.GeneralIso.Parse("2022-06-29T23:10:00").Value;
 
-var zone = DateTimeZoneProviders.Tzdb["Europe/Berlin"];
-var clock = SystemClock.Instance.InZone(zone);
-var end = clock.GetCurrentLocalDateTime();
+// var zone = DateTimeZoneProviders.Tzdb["Europe/Berlin"];
+// var clock = SystemClock.Instance.InZone(zone);
+// var end = clock.GetCurrentLocalDateTime();
 
-var diff = Period.Between(start, end);
+var utc = OffsetDateTimePattern.Rfc3339.Parse("2022-06-29T21:25:00Z").Value;
+var zone = DateTimeZoneProviders.Tzdb["Europe/Berlin"];
+var end = utc.InZone(zone).LocalDateTime;
+
+var diffPeriod = Period.Between(start, end, PeriodUnits.Hours | PeriodUnits.Minutes | PeriodUnits.Seconds);
+var diffDuration = diffPeriod.ToDuration();
 
 Console.WriteLine($"Start is {start}");
 Console.WriteLine($"End is {end}");
-Console.WriteLine($"Diff is {diff.ToDuration()}");
+Console.WriteLine($"Diff period is {diffPeriod}");
+Console.WriteLine($"Diff duration is {diffDuration}");
 
 // TODO: Test effects of dst transitions etc
