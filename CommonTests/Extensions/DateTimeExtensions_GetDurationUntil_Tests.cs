b427a61308215374ef2510a81de09780290a54cd
Dennis Renz <dennis@r-renz.de>
2022-07-02 22:03:01 +0200

    Test against all time zone ids

--- CommonTests/Extensions/DateTimeExtensions_GetDurationUntil_Tests.cs
+++ CommonTests/Extensions/DateTimeExtensions_GetDurationUntil_Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using Common.Extensions;
 using FluentAssertions;
@@ -12,13 +13,17 @@ namespace CommonTests.Extensions;
 [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Improved readability")]
 public class DateTimeExtensions_GetDurationUntil_Tests
 {
+    private static class ValueSources
+    {
+        public static IEnumerable<string> AllTzdbTimeZoneIds => DateTimeZoneProviders.Tzdb.Ids;
+    }
+    
     [Test]
     [Description("Duration is calculated from a date time value of arbitrary kind to a zoned date time value of " +
                  "arbitrary zone. The correct result should be returned.")]
     public void Duration_is_calculated_correctly_for_date_time_value_of_arbitrary_kind(
         [Values] DateTimeKind kind,
-        // TODO: Test against all time zones available in tzdb
-        [Values("Europe/Berlin", "UTC", "UTC+02", "America/Los_Angeles")] string timeZoneId)
+        [ValueSource(typeof(ValueSources), nameof(ValueSources.AllTzdbTimeZoneIds))] string timeZoneId)
     {
         var start = new DateTime(2022, 01, 01, 00, 00, 00, kind);
         var timeZone = DateTimeZoneProviders.Tzdb[timeZoneId];
