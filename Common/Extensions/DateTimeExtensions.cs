0e11e81c5d31cff560feeb222eec359446af0857
Dennis Renz <dennis@r-renz.de>
2022-07-03 23:32:23 +0200

    Use default lenient resolver for transition behavior

--- Common/Extensions/DateTimeExtensions.cs
+++ Common/Extensions/DateTimeExtensions.cs
@@ -1,15 +1,17 @@
 ﻿using JetBrains.Annotations;
 using NodaTime;
 using NodaTime.Extensions;
+using NodaTime.TimeZones;
 
 namespace Common.Extensions;
 
 [PublicAPI]
 public static class DateTimeExtensions
 {
-    public static Duration GetDurationUntil(this DateTime start, ZonedDateTime end)
+    public static Duration GetDurationUntil(this DateTime start, ZonedDateTime end,
+        ZoneLocalMappingResolver? resolver = null)
     {
-        var startInZone = start.ToLocalDateTime().InZoneStrictly(end.Zone);
+        var startInZone = start.ToLocalDateTime().InZone(end.Zone, resolver ?? Resolvers.LenientResolver);
         return end - startInZone;
     }
 }
--- CommonTests/Extensions/DateTimeExtensions_GetDurationUntil_Tests.cs
+++ CommonTests/Extensions/DateTimeExtensions_GetDurationUntil_Tests.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using Common.Extensions;
 using FluentAssertions;
 using NodaTime;
+using NodaTime.Testing.TimeZones;
 using NUnit.Framework;
 using DateTimeExtensions = Common.Extensions.DateTimeExtensions;
 
@@ -18,7 +19,8 @@ public class DateTimeExtensions_GetDurationUntil_Tests
         // TODO: Add UTC and UTC +/- offset to list
         public static IEnumerable<string> AllTzdbTimeZoneIds => DateTimeZoneProviders.Tzdb.Ids;
     }
-    
+
+    // TODO: Create date time values from pattern
     [Test]
     [Description("Duration is calculated from a date time value of arbitrary kind to a zoned date time value of " +
                  "arbitrary zone. The correct result should be returned.")]
@@ -32,6 +34,46 @@ public class DateTimeExtensions_GetDurationUntil_Tests
 
         start.GetDurationUntil(end).Should().Be(Duration.FromHours(1));
     }
+
+    // TODO: Create date time values from pattern
+    // TODO: Refine description and name, what should happen?
+    [Test]
+    [Description("Duration is calculated from a date time value in a backward transition, i.e. ambiguous.")]
+    public void Backward_transition()
+    {
+        var transition = Instant.FromUtc(
+            2022, 01, 01,
+            01, 00);
+        var zone = new SingleTransitionDateTimeZone(transition, 0, -1);
+
+        var start = new DateTime(
+            2022, 01, 01,
+            00, 30, 00);
+        var end = new LocalDateTime(
+            2022, 01, 01,
+            01, 30, 00).InZoneLeniently(zone);
+        
+        start.GetDurationUntil(end).Should().Be(Duration.FromHours(2));
+    }
     
-    // TODO: Test for time zone transition
+    // TODO: Create date time values from pattern
+    // TODO: Refine description and name, what should happen?
+    [Test]
+    [Description("Duration is calculated from a date time value in a forward transition, i.e. non existent.")]
+    public void Forward_transition()
+    {
+        var transition = Instant.FromUtc(
+            2022, 01, 01,
+            01, 00);
+        var zone = new SingleTransitionDateTimeZone(transition, 0, 1);
+
+        var start = new DateTime(
+            2022, 01, 01,
+            01, 30, 00);
+        var end = new LocalDateTime(
+            2022, 01, 01,
+            03, 00, 00).InZoneLeniently(zone);
+        
+        start.GetDurationUntil(end).Should().Be(Duration.FromMinutes(30));
+    }
 }
